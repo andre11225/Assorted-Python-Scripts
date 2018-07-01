2b019ccb26ef501c5ab4e7edb4db968042291801
andre11225 <andre11225@yahoo.com>
2018-07-01 19:06:41 -0400

    Add files via upload

---
+++ sendemail.py
@@ -0,0 +1,30 @@
+# This program simply sends a simple email with text, that's all. #
+
+import smtplib
+
+# Source email address
+fromaddr = ''
+
+# Destination email address
+toaddrs = ''
+
+# Message being sent to the receivor
+msg = ''
+
+# Login info of the sender's email account
+username = ''
+password = ''
+
+
+# The server address being used to send the email, must be a valid server
+
+# Google's server
+server = smtplib.SMTP('smtp.gmail.com:587')
+
+
+# Process that sends email
+server.ehlo()
+server.starttls()
+server.login(username,password)
+server.sendmail(fromaddr, toaddrs, msg)
+server.quit()
