84320abd3407549380cbf1dd14e6c7500dd7a182
andre11225 <andre11225@yahoo.com>
2018-07-01 19:05:31 -0400

    Add files via upload

---
+++ MessingWithTextFiles.py
@@ -0,0 +1,6 @@
+#This program is for opening/creating text files with custom text in it. . .
+
+with open ('C:\\Users\Andre\Documents\\RunningAnotherTest.txt', 'w') as file:
+    file.write('This is the second iteration of the file.')
+    file.write('\nLets see if this works.')
+    file.close()
